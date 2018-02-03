269e25683070b240a39e9781329c936ea09210b3
ybusad <stxxhwf@yeah.net>
2018-02-03 18:20:09 +0800

    hello world

---
+++ index.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+let server = app.listen(3000, () => {
+  let host = server.address().address;
+  let port = server.address().port;
+
+  console.log(`Server is listening at http://${host}:${port}`);
+});
